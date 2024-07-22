14e486b75e3f6bd32b3656272100873eeaa6c73f
José Carlos García Briseño <79120485+Carlos-Brisenio@users.noreply.github.com>
2024-07-22 15:56:35 -0600

    modified:   menuAdministrador/cartas/cartas.php 	new file:   menuAdministrador/cartas/stylesCar.css

--- menuAdministrador/cartas/cartas.php
+++ menuAdministrador/cartas/cartas.php
@@ -74,7 +74,7 @@
     
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="/pruebas/menuUsuario/styleMenu.css">
-    <link rel="stylesheet" href="stylesEst.css">
+    <link rel="stylesheet" href="/stylesCar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     
     <!----===== Boxicons CSS ===== -->
